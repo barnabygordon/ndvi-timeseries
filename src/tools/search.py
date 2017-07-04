0cf7e77a4601df5ee4e454559eb125274eece62e
BarneyGordon <barnaby.gordon@hummingbirdtechgroup.com>
2017-07-04 15:25:51 +0100

    typing

--- src/tools/search.py
+++ src/tools/search.py
@@ -1,5 +1,5 @@
-import requests
 from osgeo import gdal
+import requests
 import numpy as np
 import urllib3
 import math
@@ -7,17 +7,18 @@ import math
 from src.tools import gis
 
 API_URL = 'https://api.developmentseed.org/landsat'
+AWS_LS8_URL = 'http://landsat-pds.s3.amazonaws.com/L8'
 WGS84_EPSG = 4326
 
 
 class NDVITimeseries:
-    def __init__(self, longitude, latitude, min_cloud=0, max_cloud=100):
+    def __init__(self, longitude: float, latitude: float, min_cloud: int=0, max_cloud: int=100):
         self.longitude = longitude
         self.latitude = latitude
         self.min_cloud = min_cloud
         self.max_cloud = max_cloud
 
-    def search_landsat(self, min_date, max_date, limit):
+    def search_landsat(self, min_date: str, max_date: str, limit: int) -> dict:
         query = self._query_builder(min_date, max_date, limit)
         response = requests.get(query)
 
@@ -61,29 +62,27 @@ class NDVITimeseries:
 
         return "{}?search={}&limit={}".format(API_URL, query, limit)
 
-    def _get_ndvi_from_aws(self, scene_id):
-
+    def _get_ndvi_from_aws(self, scene_id: str) -> float:
         path, row = scene_id[3:6], scene_id[6:9]
 
         bands = [5, 4]
-        data = []
 
         meta_data = self._get_aws_meta(scene_id, path, row)
 
+        data = []
         for band in bands:
-
             url = self._aws_url_builder(scene_id, path, row, band)
 
-            src = gdal.Open(url)
-            prj = src.GetProjection()
-            epsg_out = prj.split('"EPSG",')[-1].strip(']').strip('"')
+            image_dataset = gdal.Open(url)
+            prorjection = image_dataset.GetProjection()
+            epsg_out = prorjection.split('"EPSG",')[-1].strip(']').strip('"')
 
             lng2, lat2 = gis.convert_coords(self.longitude, self.latitude, WGS84_EPSG, epsg_out)
-            x, y = gis.world_to_pixel(src, lng2, lat2)
+            x, y = gis.world_to_pixel(image_dataset, lng2, lat2)
 
-            dn = src.ReadAsArray(y, x, 1, 1).astype('float32')
+            digital_number = image_dataset.ReadAsArray(y, x, 1, 1).astype('float32')
 
-            reflectance = self._radiance2reflectance(dn, band, meta_data)
+            reflectance = self._radiance2reflectance(digital_number, band, meta_data)
 
             data.append(reflectance)
 
@@ -92,26 +91,27 @@ class NDVITimeseries:
         return ndvi
 
     @staticmethod
-    def _get_aws_meta(scene_id, path, row):
-        meta_url = 'http://landsat-pds.s3.amazonaws.com/L8/{}/{}/{}/{}_MTL.txt'.format(path, row, scene_id, scene_id)
+    def _get_aws_meta(scene_id: str, path: int, row: int) -> str:
+        meta_url = '{}/{}/{}/{}/{}_MTL.txt'.format(
+            AWS_LS8_URL, path, row, scene_id, scene_id)
         # TODO update to Python3
         meta_data = urllib3.urlopen(meta_url).readlines()
 
         return meta_data
 
     @staticmethod
-    def _aws_url_builder(scene_id, path, row, band):
-        url = '/vsicurl/http://landsat-pds.s3.amazonaws.com/L8/{}/{}/{}/{}_B{}.TIF'.format(
-            path, row, scene_id, scene_id, band)
+    def _aws_url_builder(scene_id: str, path: int, row: int, band: int) -> str:
+        url = '/vsicurl/{}/{}/{}/{}/{}_B{}.TIF'.format(
+            AWS_LS8_URL, path, row, scene_id, scene_id, band)
 
         return url
 
-    def _radiance2reflectance(self, dn, band, meta_data):
+    def _radiance2reflectance(self, digital_number: float, band: int, meta_data: str) -> float:
         """ Conversion Top Of Atmosphere planetary reflectance
         REF: http://landsat.usgs.gov/Landsat8_Using_Product.php
         Following function based on work by Vincent Sarago:
         https://github.com/vincentsarago/landsatgif/blob/master/landsat_gif.py
-        :param dn:
+        :param digital_number: digital number value
         :param band:
         :param meta_data:
         :return:
@@ -121,12 +121,12 @@ class NDVITimeseries:
         ap = float(self._landsat_extract_mtl(meta_data, "REFLECTANCE_ADD_BAND_{}".format(band)))
         se = math.radians(float(self._landsat_extract_mtl(meta_data, "SUN_ELEVATION")))
 
-        reflect_toa = (np.where(dn > 0, (mp * dn + ap) / math.sin(se), 0))
+        reflect_toa = (np.where(digital_number > 0, (mp * digital_number + ap) / math.sin(se), 0))
 
         return reflect_toa
 
     @staticmethod
-    def _landsat_extract_mtl(meta_data, param):
+    def _landsat_extract_mtl(meta_data: list, param: str) -> str:
         """ Extract Parameters from MTL file """
         for line in meta_data:
             data = line.split(' = ')
